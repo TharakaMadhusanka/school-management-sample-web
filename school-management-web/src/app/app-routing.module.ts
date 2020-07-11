e36e97afe13a1539deac809f634e663937763aef
TharakaMadhusanka <tharakalearn@gmail.com>
2020-07-11 10:44:20 +0530

    added home module

--- school-management-web/src/app/app-routing.module.ts
+++ school-management-web/src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+  {
     path: 'school',
     loadChildren: () => import('./school-details/school-details.module').then(m => m.SchoolDetailsModule)
   }
